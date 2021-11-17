1f81d02b39c6f392f9e9761af14df9f8c64e7412
Alan Jhonnes <alanjhonnes@gmail.com>
2021-11-17 18:26:56 -0300

    feat: initial setup

---
+++ src/index.ts
@@ -0,0 +1,23 @@
+import express from 'express';
+import dotenv from 'dotenv';
+
+const PORT = process.env.PORT || 3000;
+const HOST = '127.0.0.1';
+
+
+const envFound = dotenv.config();
+if (envFound.error) {
+  // This error should crash whole process
+
+  throw new Error("⚠️  Couldn't find .env file  ⚠️");
+}
+
+
+// App
+const app = express();
+app.get('/', (req, res) => {
+    res.send('Hello world\n');
+});
+app.listen(PORT, () => {
+    console.log(`Running on http://${HOST}:${PORT}`);
+});
